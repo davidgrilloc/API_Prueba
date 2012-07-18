da9d1d425800f1c28ae3f3ea611f6225694110dd
davidgrilloc <davidgroovey>
2012-07-17 22:05:37 -0500

    '1ercommit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APIPrueba::Application.config.secret_token = 'b75cb22e84f374cbf8c6ac3f12f3141a61cb7bccf5a878767dca6818a7c4eae03abf151df1832cdce3126288d537e5bfeba671a7e022a40cdc0bedc22302a1c8'
